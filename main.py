30789c06ceb219124319240708a3a4bb08561209
Lawson Robert <lawsonboevirobert@gmail.com>
2025-12-03 17:24:05 +0100

    features: picture_position implementation

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ pex = Image.open("pex.jpg")
 print(pex)
 pex.save("new_pex.png")
 
+
+largeurs = pex.size[0]
+hauteurs =pex.size[1]
+
+def img_position(x,y,image):
+    pixel = image.getpixel((x,y))
+    return print(pixel)
+
+img_position(1,4,pex)
+    
+
+        
